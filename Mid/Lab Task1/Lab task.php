56bca8e6f53ea53a3aca496d57756135cb6366e6
Mursalin_Leon <121357813+Mursalin95@users.noreply.github.com>
2025-11-02 12:36:28 +0600

    Add patient registration form in Lab task.php

--- Mid/Lab Task1/Lab task.php
+++ Mid/Lab Task1/Lab task.php
@@ -1,1 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h1><b>Clinic Patient Registration</b></h1>
+    <table>
+    <tr><td>
+    Full Name: <br> 
+    <input type="text"><br></td></tr>
+    <tr><td>Age:<br>
+    <input type="number"><br></td> </tr>
+    <tr><td>
+    Phone Number:<br>
+    <input type="number"> <br></td></tr>
+   <tr><td>
+    Email Address:<br>
+    <input type="text"> <br></td></tr>
 
+    <tr><td>
+    Insurance Provider:<br>
+    <select> 
+        <option>Select Provider </option>
+        <option>Provider1</option>
+    <br></td></tr>
+
+    <tr><td>
+    Insurance Policy Number:<br>
+    <input type="number"> </td></tr></table></center>
+
+    <center><h2><b>Additional Information</b></h2>
+    <table>
+    <tr><td>
+    Username:<br>
+    <input type="text"> <br></td></tr>
+
+    <tr><td>
+    Password:</td><br>
+    <input type="text"> <br></td></tr>
+
+    <tr><td>
+    Confirm Password:<br>
+    <input type="text"> <br></td></tr>
+
+    <tr><td><input type ="submit" value="Register" ></td></tr>
+  </table></center>
+</body>
+</html>
